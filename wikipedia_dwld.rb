7d17b581b6e9487e20e15eb289c41602c2f07dc6
maxtiff <travismay@gmail.com>
2014-01-19 23:31:41 -0600

    Modify wikipedia_dwld to use variables for file names

--- wikipedia_dwld.rb
+++ wikipedia_dwld.rb
@@ -5,12 +5,17 @@ remote_base_url = "http://en.wikipedia.org/wiki/"
 remote_page_name = "Samurai_champloo"
 remote_full_url = remote_base_url + remote_page_name
 
+puts "Downloading from: " + remote_full_url
+
 # Declare variable to store html
 remote_data = open(remote_full_url).read
 
 # Create local file that will contain html from remote_data
-local_file = open("downloaded_page.html", "w")
+local_file_name = remote_page_name + "_local_copy.html"
+
+puts "Writing to: " + local_file_name
 
 # Write html to local disk
+local_file = open(local_file_name, "w")
 local_file.write(remote_data)
 local_file.close
